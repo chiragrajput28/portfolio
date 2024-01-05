0b9d8db3ce7cadfa183aa15b69a0a44ddb983bcc
chirag.ccdps28@gmail.com <chirag.ccdps28@gmail.com>
2024-01-05 15:59:58 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { BrowserRouter } from 'react-router-dom'
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from './components'
 function App() {
   const [count, setCount] = useState(0)
-
+  const styleWithoutPadding = {
+    margin: 0,
+    padding: 0,
+  };
   return (
     <div>
       <BrowserRouter>
-        <div className = "relative z-0  primary">
-          <div className = "bg-hero-pattern bg-cover bg-no-repeat bg-center">
-            <Navbar/>
-            <Hero/>
-          </div>
+        <div style = {styleWithoutPadding}>
+          <Navbar/>
+          <Hero/>
           <About/>
           <Experience/>
           <Tech/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,107 @@
-import React, { useEffect, useState  } from 'react'
-import { Link } from 'react-router-dom'
-import { styles } from "../styles"
-import { navLinks } from "../constants"
-import { logo, menu, close } from '../assets'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>hi</div>
-  )
-}
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
 
-export default Navbar
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      if (scrollTop > 100) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  const screenHeight = window.innerHeight;
+  const screenWidth = window.innerWidth;
+  return (
+    <nav>
+      <div className='w-full flex justify-between items-center'>
+        <Link
+          to='/'
+          className='flex items-center gap-2'
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={logo} alt='logo' style={{
+          height: `${screenHeight}px`,  
+          width: `${screenWidth}px`,    
+          border: '1px solid black',     
+        }} />
+          <p className='text-white text-[18px] font-bold cursor-pointer flex '>
+            Chirag Rajput &nbsp;
+            <span className='sm:block hidden'> | The Corporate Monk</span>
+          </p>
+        </Link>
+
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {navLinks.map((nav) => (
+            <li
+              key={nav.id}
+              className={`${
+                active === nav.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(nav.title)}
+            >
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className='sm:hidden flex flex-1 justify-end items-center'>
+          <img
+            src={toggle ? close : menu}
+            alt='menu'
+            className='w-[28px] h-[28px] object-contain'
+            onClick={() => setToggle(!toggle)}
+          />
+
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className='list-none flex justify-end items-start flex-1 flex-col gap-4'>
+              {navLinks.map((nav) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-secondary"
+                  }`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(nav.title);
+                  }}
+                >
+                  <a href={`#${nav.id}`}>{nav.title}</a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+  // <>
+  //  <img src={logo} alt='logo' style={{
+  //         height: `${screenHeight}px`,  
+  //         width: `${screenWidth}px`     
+  //       }} />
+  // </>
+};
+
+export default Navbar;
